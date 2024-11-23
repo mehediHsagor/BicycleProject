39970fd6cee0702e63e293dd43a6a54259d850c5
Mehedi Hasan Sagor <mehedisagor2001@gmail.com>
2024-11-24 03:28:15 +0600

    2nd

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 5000
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+app.use(cors());
 
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
+export default app;
--- src/Server.ts
+++ src/Server.ts
@@ -1,11 +1,50 @@
-const mongoose = require('mongoose');
+import app from './App';
+import config from './App/config';
+import mongoose from 'mongoose';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 async function main() {
-    await mongoose.connect('process.env.DATABASE_URL');
-  
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
   }
-
-
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`)
-  })
+}
+main();
